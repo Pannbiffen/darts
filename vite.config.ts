1341acc4af48b59c484c32fe12bf7ec46dc33b41
Martin Olsson <Martin.mupp@gmail.com>
2026-03-26 00:07:45 +0100

    Resolve 404 error for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -29,20 +29,20 @@ export default defineConfig(({ mode }) => {
           theme_color: "#0f172a",
           background_color: "#0f172a",
           display: "standalone",
-          start_url: "/",
+          start_url: "/darts/",
           icons: [
             {
-              src: "/icon-192.png",
+              src: "icon-192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/icon-512.png",
+              src: "icon-512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "/icon-512.png",
+              src: "icon-512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "maskable",
